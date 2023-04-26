4bf8d38954367e7954dad2163bcbb3bbf95147be
vagrant <vagrant@markethaven>
2023-04-26 11:04:25 +0000

    add-passport-branch: add passport package to the project and create a Uuid trait

---
+++ src/shared/Traits/Uuid.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Shared\Traits;
+
+use Illuminate\Support\Str;
+
+trait Uuid
+{
+    protected static function boot()
+    {
+        parent::boot();
+        static::creating(function ($model) {
+            if (empty($model->{$model->getKeyName()})) {
+                $model->{$model->getKeyName()} = Str::orderedUuid()->toString();
+            }
+        });
+    }
+}
